ab5601f4e4d5e64faa048a973dd68eb2c9d6ddd3
Shreyas Nidugala <shreyasnidugala@gmail.com>
2022-12-06 07:03:05 +0530

    document_screen UI

--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gdocs_flutter/colors.dart';
 
 class DocumentScreen extends ConsumerStatefulWidget {
   final String id;
@@ -10,11 +11,73 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titleController =
+      TextEditingController(text: "Untitled Document");
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: kWhiteColor,
+          elevation: 0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.lock,
+                  size: 16,
+                ),
+                label: const Text("Share"),
+                style: ElevatedButton.styleFrom(primary: kBlueColor),
+              ),
+            ),
+          ],
+          title: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/docs-logo.png',
+                  height: 40,
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                    width: 180,
+                    child: TextField(
+                      controller: titleController,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: kBlueColor,
+                          ),
+                        ),
+                        contentPadding: EdgeInsets.only(
+                          left: 10,
+                        ),
+                      ),
+                    ))
+              ],
+            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: kGreyColor, width: 0.1)),
+            ),
+          ),
+        ),
         body: Center(
-      child: Text(widget.id),
-    ));
+          child: Text(widget.id),
+        ));
   }
 }
